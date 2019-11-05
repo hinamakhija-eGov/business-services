c26849d0b84e7c9202e28cf89ef69675eb4a3b07
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2019-11-05 23:21:53 +0530

    constructer added for billv2

--- billing-service/src/main/java/org/egov/demand/model/BillV2.java
+++ billing-service/src/main/java/org/egov/demand/model/BillV2.java
@@ -10,10 +10,14 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class BillV2 {
 
