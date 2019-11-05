b7a0a32adf72e46df197326521e5c261c347d4e9
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2019-11-05 23:21:53 +0530

    constructer added for billv2 models i

--- billing-service/src/main/java/org/egov/demand/model/BillAccountDetailV2.java
+++ billing-service/src/main/java/org/egov/demand/model/BillAccountDetailV2.java
@@ -4,14 +4,18 @@ import java.math.BigDecimal;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * BillAccountDetail
  */
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class BillAccountDetailV2   {
 	
--- billing-service/src/main/java/org/egov/demand/model/BillDetailV2.java
+++ billing-service/src/main/java/org/egov/demand/model/BillDetailV2.java
@@ -8,13 +8,17 @@ import javax.validation.Valid;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * BillDetail
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class BillDetailV2 {
 
