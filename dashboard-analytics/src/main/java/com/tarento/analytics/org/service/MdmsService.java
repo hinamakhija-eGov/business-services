ec953064bb6959b0002730abe9cc8106c1dcf325
pritha-tarento <pritha.chattopadhyay@tarento.com>
2019-12-23 20:40:27 +0530

    mdms sevice: default DDRs

--- dashboard-analytics/src/main/java/com/tarento/analytics/org/service/MdmsService.java
+++ dashboard-analytics/src/main/java/com/tarento/analytics/org/service/MdmsService.java
@@ -39,19 +39,23 @@ public class MdmsService {
     public void loadMdmsService() throws Exception{
 
         JsonNode requestInfo = mapper.readTree(REQUEST_INFO_STR);
-        JsonNode response = restService.post(mdmsServiceSearchUri, "", requestInfo);
-        ArrayNode tenants = (ArrayNode) response.findValues(Constants.MDMSKeys.TENANTS).get(0);
+        try {
+            JsonNode response = restService.post(mdmsServiceSearchUri, "", requestInfo);
+            ArrayNode tenants = (ArrayNode) response.findValues(Constants.MDMSKeys.TENANTS).get(0);
 
 
-        for(JsonNode tenant : tenants) {
-            JsonNode ddrCode = tenant.findValue(Constants.MDMSKeys.DISTRICT_CODE);
-            JsonNode ddrName = tenant.findValue(Constants.MDMSKeys.DDR_NAME);
+            for(JsonNode tenant : tenants) {
+                JsonNode ddrCode = tenant.findValue(Constants.MDMSKeys.DISTRICT_CODE);
+                JsonNode ddrName = tenant.findValue(Constants.MDMSKeys.DDR_NAME);
 
-            if (!ddrTenantMapping.containsKey(ddrCode.asText())){
-                ddrTenantMapping.put(ddrCode.asText(), ddrName.asText());
+                if (!ddrTenantMapping.containsKey(ddrCode.asText())){
+                    ddrTenantMapping.put(ddrCode.asText(), ddrName.asText());
+                }
             }
+        } catch (Exception e){
+            getDefaultMapping();
+            logger.error("Loading Mdms service error: "+e.getMessage()+" :: loaded default DDRs");
         }
-
         logger.info("ddrTenantMapping = "+ddrTenantMapping);
     }
 
@@ -59,5 +63,33 @@ public class MdmsService {
         return ddrTenantMapping.getOrDefault(ddrCode, "");
     }
 
+    private void getDefaultMapping(){
+
+        ddrTenantMapping.put("1", "Amritsar-DDR");
+        ddrTenantMapping.put("2", "Patiala-DDR");
+        ddrTenantMapping.put("3", "Bathinda-DDR");
+        ddrTenantMapping.put("4", "Ferozepur-DDR");
+        ddrTenantMapping.put("5", "Ludhiana-DDR");
+        ddrTenantMapping.put("6", "Ferozepur-DDR");
+        ddrTenantMapping.put("7", "Ferozepur-DDR");
+        ddrTenantMapping.put("8", "Amritsar-DDR");
+        ddrTenantMapping.put("9", "Jalandhar-DDR");
+        ddrTenantMapping.put("10", "Jalandhar-DDR");
+
+        ddrTenantMapping.put("11", "Jalandhar-DDR");
+        ddrTenantMapping.put("12", "Ludhiana-DDR");
+        ddrTenantMapping.put("13", "Bathinda-DDR");
+        ddrTenantMapping.put("14", "Ferozepur-DDR");
+        ddrTenantMapping.put("15", "Patiala-DDR");
+        ddrTenantMapping.put("16", "Bathinda-DDR");
+        ddrTenantMapping.put("17", "Jalandhar-DDR");
+        ddrTenantMapping.put("18", "Pathankot-MC");
+        ddrTenantMapping.put("19", "Patiala-DDR");
+        ddrTenantMapping.put("20", "Ludhiana-DDR");
+        ddrTenantMapping.put("21", "Patiala-DDR");
+        ddrTenantMapping.put("22", "Bathinda-DDR");
+        ddrTenantMapping.put("140001", "Ludhiana-DDR");
+
+    }
 
 }
