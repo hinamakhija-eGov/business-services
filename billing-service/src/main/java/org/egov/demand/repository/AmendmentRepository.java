2efc8aa25de543e4ae8db5e6f9788706f39e207f
kavi_elrey@1993 <25226238+kavi-egov@users.noreply.github.com>
2021-03-01 21:10:35 +0530

    jdbc template changed to namedparam (#254)

--- billing-service/src/main/java/org/egov/demand/repository/AmendmentRepository.java
+++ billing-service/src/main/java/org/egov/demand/repository/AmendmentRepository.java
@@ -5,8 +5,6 @@ import static org.egov.demand.repository.querybuilder.AmendmentQueryBuilder.AMEN
 import static org.egov.demand.repository.querybuilder.AmendmentQueryBuilder.AMENDMENT_UPDATE_QUERY;
 import static org.egov.demand.repository.querybuilder.AmendmentQueryBuilder.DOCUMET_INSERT_QUERY;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,9 +19,9 @@ import org.egov.demand.repository.querybuilder.AmendmentQueryBuilder;
 import org.egov.demand.repository.rowmapper.AmendmentRowMapper;
 import org.egov.demand.util.Util;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BatchPreparedStatementSetter;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,7 +32,7 @@ public class AmendmentRepository {
 	private Util util;
 	
 	@Autowired
-	private JdbcTemplate jdbcTemplate;
+	private NamedParameterJdbcTemplate namedJdbcTemplate;
 	
 	@Autowired
 	private AmendmentRowMapper amendmentRowMapper;
@@ -47,116 +45,118 @@ public class AmendmentRepository {
 		
 		Amendment amendment = amendmentRequest.getAmendment();
 
-		AuditDetails auditDetails = amendment.getAuditDetails();
-		jdbcTemplate.update(AMENDMENT_INSERT_QUERY, new PreparedStatementSetter() {
-
-			@Override
-			public void setValues(PreparedStatement ps) throws SQLException {
-
-				ps.setString(1, amendment.getId());
-				ps.setString(2, amendment.getTenantId());
-				ps.setString(3, amendment.getAmendmentId());
-				ps.setString(4, amendment.getBusinessService());
-				ps.setString(5, amendment.getConsumerCode());
-				ps.setString(6, amendment.getAmendmentReason().toString());
-				ps.setString(7, amendment.getReasonDocumentNumber());
-				ps.setString(8, amendment.getStatus().toString());
-				ps.setObject(9, amendment.getEffectiveTill());
-				ps.setObject(10, amendment.getEffectiveFrom());
-				ps.setString(11, amendment.getAmendedDemandId());
-				ps.setString(12, auditDetails.getCreatedBy());
-				ps.setLong  (13, auditDetails.getCreatedTime());
-				ps.setString(14, auditDetails.getLastModifiedBy());
-				ps.setLong  (15, auditDetails.getLastModifiedTime());
-				ps.setObject(16, util.getPGObject(amendment.getAdditionalDetails()));
-
-			}
-		});
-		
+		namedJdbcTemplate.update(AMENDMENT_INSERT_QUERY, getAmendmentSqlParameter(amendmentRequest));
 		saveTaxDetail(amendment.getDemandDetails(), amendment.getId());
 		savedocs(amendment.getDocuments(), amendment.getId());
-		
-		// save document
 	}
 	
 	private void saveTaxDetail(List<DemandDetail> demandDetails, String amendmentId) {
 
-		jdbcTemplate.batchUpdate(AMENDMENT_TAXDETAIL_INSERT_QUERY, new BatchPreparedStatementSetter() {
-			
-			@Override
-			public void setValues(PreparedStatement ps, int rowNum) throws SQLException {
-
-				DemandDetail detail = demandDetails.get(rowNum);
-				ps.setString(1, detail.getId());
-				ps.setString(2, amendmentId);
-				ps.setString(3, detail.getTaxHeadMasterCode());
-				ps.setBigDecimal(4, detail.getTaxAmount());
-			}
-			
-			@Override
-			public int getBatchSize() {
-				return demandDetails.size();
-			}
-		});
+		List<MapSqlParameterSource> sqlParameterSources = getSqlParameterListForTaxDetails(demandDetails, amendmentId);
+		namedJdbcTemplate.batchUpdate(AMENDMENT_TAXDETAIL_INSERT_QUERY, sqlParameterSources.toArray(new MapSqlParameterSource[0]));
 	}
 	
 	private void savedocs(List<Document> documents, String amendmentId) {
-		
-		jdbcTemplate.batchUpdate(DOCUMET_INSERT_QUERY, new BatchPreparedStatementSetter() {
-			
-			@Override
-			public void setValues(PreparedStatement ps, int rowNum) throws SQLException {
 
-				Document doc = documents.get(rowNum);
-				ps.setString(1, doc.getId());
-				ps.setString(2, amendmentId); // amendmentId as demandId
-				ps.setString(3, doc.getDocumentType());
-				ps.setString(4, doc.getFileStoreId());
-				ps.setString(5, doc.getDocumentUid());
-				ps.setString(6, "ACTIVE"); // hard-coded to active since no change possible
-				
-			}
-			
-			@Override
-			public int getBatchSize() {
-				return documents.size();
-			}
-		});
+		List<MapSqlParameterSource> sqlParameterSources = getSqlParameterListForDocs(documents, amendmentId);
+		namedJdbcTemplate.batchUpdate(DOCUMET_INSERT_QUERY, sqlParameterSources.toArray(new MapSqlParameterSource[0]));
 	}
 
 	public List<Amendment> getAmendments (AmendmentCriteria amendmentCriteria) {
 
-		List<Object> psValues = new ArrayList<>();
-		String searchQuery = amendmentQueryBuilder.getSearchQuery(amendmentCriteria, psValues);
-		return jdbcTemplate.query(searchQuery, psValues.toArray(), amendmentRowMapper);
+		MapSqlParameterSource searchParamMap = new MapSqlParameterSource();
+		String searchQuery = amendmentQueryBuilder.getSearchQuery(amendmentCriteria, searchParamMap);
+		return namedJdbcTemplate.query(searchQuery, searchParamMap, amendmentRowMapper);
 	}
 
 	@Transactional
 	public void updateAmendment(List<AmendmentUpdate> amendmentUpdates) {
-			
-			
-			jdbcTemplate.batchUpdate(AMENDMENT_UPDATE_QUERY, new BatchPreparedStatementSetter() {
 
-				@Override
-				public void setValues(PreparedStatement ps, int rowNum) throws SQLException {
-
-					AmendmentUpdate amendmentUpdate = amendmentUpdates.get(rowNum);
-					AuditDetails auditDetails = amendmentUpdate.getAuditDetails();
-					
-					ps.setString(1, amendmentUpdate.getStatus().toString());
-					ps.setString(2, amendmentUpdate.getAmendedDemandId());
-					ps.setString(3, auditDetails.getLastModifiedBy());
-					ps.setLong(4, auditDetails.getLastModifiedTime());
-					ps.setObject(5, util.getPGObject(amendmentUpdate.getAdditionalDetails()));
-					ps.setString(6, amendmentUpdate.getTenantId());
-					ps.setString(7, amendmentUpdate.getAmendmentId());
-				}
-
-				@Override
-				public int getBatchSize() {
-					return amendmentUpdates.size();
-				}
-
-			});
-		}
+		List<MapSqlParameterSource> sqlParameterSources = getSqlParameterListForAmendmentUpdate(amendmentUpdates);
+		namedJdbcTemplate.batchUpdate(AMENDMENT_UPDATE_QUERY, sqlParameterSources.toArray(new MapSqlParameterSource[0]));
 	}
+
+	/*
+	 * map-sql-parameter construction methods
+	 */
+	public SqlParameterSource getAmendmentSqlParameter(AmendmentRequest amendmentRequest) {
+
+		MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+		Amendment amendment = amendmentRequest.getAmendment();
+		AuditDetails auditDetails = amendment.getAuditDetails();
+
+		mapSqlParameterSource.addValue("id", amendment.getId());
+		mapSqlParameterSource.addValue("tenantid", amendment.getTenantId());
+		mapSqlParameterSource.addValue("amendmentid", amendment.getAmendmentId());
+		mapSqlParameterSource.addValue("businessservice", amendment.getBusinessService());
+		mapSqlParameterSource.addValue("consumercode", amendment.getConsumerCode());
+		mapSqlParameterSource.addValue("amendmentreason", amendment.getAmendmentReason().toString());
+		mapSqlParameterSource.addValue("reasondocumentnumber", amendment.getReasonDocumentNumber());
+		mapSqlParameterSource.addValue("status", amendment.getStatus().toString());
+		mapSqlParameterSource.addValue("effectivetill", amendment.getEffectiveTill());
+		mapSqlParameterSource.addValue("effectivefrom", amendment.getEffectiveFrom());
+		mapSqlParameterSource.addValue("amendeddemandid", amendment.getAmendedDemandId());
+		mapSqlParameterSource.addValue("createdby", auditDetails.getCreatedBy());
+		mapSqlParameterSource.addValue("createdtime", auditDetails.getCreatedTime());
+		mapSqlParameterSource.addValue("lastmodifiedby", auditDetails.getLastModifiedBy());
+		mapSqlParameterSource.addValue("lastmodifiedtime", auditDetails.getLastModifiedTime());
+		mapSqlParameterSource.addValue("additionaldetails", util.getPGObject(amendment.getAdditionalDetails()));
+
+		return mapSqlParameterSource;
+	}
+
+	private List<MapSqlParameterSource> getSqlParameterListForTaxDetails(List<DemandDetail> demandDetails,
+			String amendmentId) {
+
+		List<MapSqlParameterSource> taxDetailParamMapList = new ArrayList<>();
+		for (DemandDetail detail : demandDetails) {
+
+			MapSqlParameterSource taxDetailParamMap = new MapSqlParameterSource();
+			taxDetailParamMap.addValue("id", detail.getId());
+			taxDetailParamMap.addValue("amendmentid", amendmentId);
+			taxDetailParamMap.addValue("taxheadcode", detail.getTaxHeadMasterCode());
+			taxDetailParamMap.addValue("taxamount", detail.getTaxAmount());
+			taxDetailParamMapList.add(taxDetailParamMap);
+		}
+		return taxDetailParamMapList;
+	}
+
+	private List<MapSqlParameterSource> getSqlParameterListForDocs(List<Document> documents, String amendmentId) {
+
+		List<MapSqlParameterSource> documentsParamMapList = new ArrayList<>();
+		for (Document document : documents) {
+
+			MapSqlParameterSource documentParamMap = new MapSqlParameterSource();
+			documentParamMap.addValue("id", document.getId());
+			documentParamMap.addValue("amendmentid", amendmentId);
+			documentParamMap.addValue("documenttype", document.getDocumentType());
+			documentParamMap.addValue("filestoreid", document.getFileStoreId());
+			documentParamMap.addValue("documentuid", document.getDocumentUid());
+			documentParamMap.addValue("status", "ACTIVE");
+			documentsParamMapList.add(documentParamMap);
+		}
+
+		return documentsParamMapList;
+	}
+
+	private List<MapSqlParameterSource> getSqlParameterListForAmendmentUpdate(List<AmendmentUpdate> amendmentUpdates) {
+
+		List<MapSqlParameterSource> updateParamMapList = new ArrayList<>();
+		for (AmendmentUpdate amendmentUpdate : amendmentUpdates) {
+
+			AuditDetails auditDetails = amendmentUpdate.getAuditDetails();
+			MapSqlParameterSource updateParamMap = new MapSqlParameterSource();
+
+			updateParamMap.addValue("status", amendmentUpdate.getStatus().toString());
+			updateParamMap.addValue("amendeddemandid", amendmentUpdate.getAmendedDemandId());
+			updateParamMap.addValue("lastmodifiedby", auditDetails.getLastModifiedBy());
+			updateParamMap.addValue("lastmodifiedtime", auditDetails.getLastModifiedTime());
+			updateParamMap.addValue("additionaldetails", util.getPGObject(amendmentUpdate.getAdditionalDetails()));
+			updateParamMap.addValue("tenantid", amendmentUpdate.getTenantId());
+			updateParamMap.addValue("amendmentid", amendmentUpdate.getAmendmentId());
+			updateParamMapList.add(updateParamMap);
+		}
+		return updateParamMapList;
+	}
+
+}
--- billing-service/src/main/java/org/egov/demand/repository/querybuilder/AmendmentQueryBuilder.java
+++ billing-service/src/main/java/org/egov/demand/repository/querybuilder/AmendmentQueryBuilder.java
@@ -1,32 +1,28 @@
 package org.egov.demand.repository.querybuilder;
 
-import java.util.List;
-
 import org.egov.demand.amendment.model.AmendmentCriteria;
-import org.egov.demand.util.Util;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
 @Component
 public class AmendmentQueryBuilder {
 	
-	@Autowired
-	private Util util;
-	
-	public static final String AMENDMENT_UPDATE_QUERY = "UPDATE egbs_amendment SET status=?, amendeddemandid=?, lastmodifiedby=?,"
-			+ " lastmodifiedtime=?, additionaldetails=? WHERE tenantid=? AND amendmentid=?;";
+	public static final String AMENDMENT_UPDATE_QUERY = "UPDATE egbs_amendment SET status=:status, amendeddemandid=:amendeddemandid,"
+			+ " lastmodifiedby=:lastmodifiedby,lastmodifiedtime=:lastmodifiedtime, additionaldetails=:additionaldetails "
+			+ " WHERE tenantid=:tenantid AND amendmentid=:amendmentid;";
 	
 	public static final String AMENDMENT_INSERT_QUERY = "INSERT INTO egbs_amendment (id, tenantid, amendmentid, businessservice,"
 			+ " consumercode, amendmentreason, reasondocumentnumber, status, effectivetill, effectivefrom,"
 			+ " amendeddemandid, createdby, createdtime, lastmodifiedby, lastmodifiedtime, additionaldetails) "
-			+ "	VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+			+ "	VALUES (:id, :tenantid, :amendmentid, :businessservice, :consumercode, :amendmentreason, :reasondocumentnumber, :status,"
+			+ " :effectivetill, :effectivefrom, :amendeddemandid, :createdby, :createdtime, :lastmodifiedby, :lastmodifiedtime, :additionaldetails);";
 
 	public static final String AMENDMENT_TAXDETAIL_INSERT_QUERY = "INSERT INTO egbs_amendment_taxdetail(id, amendmentid, taxheadcode, taxamount)"
-			+ " VALUES (?, ?, ?, ?);";
+			+ " VALUES (:id, :amendmentid, :taxheadcode, :taxamount);";
 
 	public static final String DOCUMET_INSERT_QUERY = "INSERT INTO egbs_document(id, amendmentid, documenttype, filestoreid, documentuid, status)"
-			+ " VALUES (?, ?, ?, ?, ?, ?);";
+			+ " VALUES (:id, :amendmentid, :documenttype, :filestoreid, :documentuid, :status);";
 	
 	public static final String AMENDMENT_SEARCH_QUERY = "SELECT amendment.id as amendmentuuid, tenantid, amendment.amendmentid as amendmentid, businessservice,"
 			+ " consumercode, amendmentreason, reasondocumentnumber, amendment.status as amendmentstatus, effectivetill,"
@@ -39,55 +35,56 @@ public class AmendmentQueryBuilder {
 			+ "	INNER JOIN egbs_document doc ON amendment.id = doc.amendmentid WHERE ";
 
 	
-	public String getSearchQuery(AmendmentCriteria amendmentCriteria, List<Object> preparedStatementValues) {
+	public String getSearchQuery(AmendmentCriteria amendmentCriteria, MapSqlParameterSource searchParamMap) {
 
 		StringBuilder queryBuilder = new StringBuilder(AMENDMENT_SEARCH_QUERY);
 
-		queryBuilder.append(" amendment.tenantid = ? ");
-		preparedStatementValues.add(amendmentCriteria.getTenantId());
+		queryBuilder.append(" amendment.tenantid = :tenantid ");
+		searchParamMap.addValue("tenantid", amendmentCriteria.getTenantId());
 		
 		if (!CollectionUtils.isEmpty(amendmentCriteria.getConsumerCode())) {
 
 			addAndClause(queryBuilder);
-			queryBuilder.append(" consumercode IN "
-					+ util.getIdsQueryForList(amendmentCriteria.getConsumerCode(), preparedStatementValues));
+			queryBuilder.append(" consumercode IN (:consumercode)");
+			searchParamMap.addValue("consumercode", amendmentCriteria.getConsumerCode());
 		}
 		
 		if (amendmentCriteria.getBusinessService() != null) {
 
 			addAndClause(queryBuilder);
-			queryBuilder.append(" businessservice=? ");
-			preparedStatementValues.add(amendmentCriteria.getBusinessService());
+			queryBuilder.append(" businessservice= :businessservice");
+			searchParamMap.addValue("businessservice", amendmentCriteria.getBusinessService());
 		}
 		
 		if (amendmentCriteria.getAmendmentId() != null) {
 
 			addAndClause(queryBuilder);
-			queryBuilder.append(" amendment.amendmentid=? ");
-			preparedStatementValues.add(amendmentCriteria.getAmendmentId());
+			queryBuilder.append(" amendment.amendmentid=:amendmentid");
+			searchParamMap.addValue("amendmentid", amendmentCriteria.getAmendmentId());
 		}
 		
 		if (amendmentCriteria.getStatus() != null) {
 
 			addAndClause(queryBuilder);
-			queryBuilder.append(" amendment.status=? ");
-			preparedStatementValues.add(amendmentCriteria.getStatus().toString());
+			queryBuilder.append(" amendment.status=:status ");
+			searchParamMap.addValue("status", amendmentCriteria.getStatus().toString());
 		}
 		
-		addPagingClause(queryBuilder, preparedStatementValues);
+		addPagingClause(queryBuilder, searchParamMap);
 		return queryBuilder.toString();
 	}
 
-	private static void addPagingClause(StringBuilder amendmentQueryBuilder, List<Object> preparedStatementValues) {
+	private static void addPagingClause(StringBuilder amendmentQueryBuilder, MapSqlParameterSource searchParamMap) {
 		
-		amendmentQueryBuilder.append(" LIMIT ? ");
-		preparedStatementValues.add(500);
-		amendmentQueryBuilder.append(" OFFSET ? ");
-		preparedStatementValues.add(0);
+		amendmentQueryBuilder.append(" LIMIT :limit ");
+		searchParamMap.addValue("limit", 500);
+		amendmentQueryBuilder.append(" OFFSET :offset");
+		searchParamMap.addValue("offset", 0);
 	}
 	
 	private static boolean addAndClause(StringBuilder queryString) {
 		queryString.append(" AND ");
 		return true;
 	}
+
 }
